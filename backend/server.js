2e2f2bfdc7d4fe5e817349e378ef0416b6a7032e
yimialmonte <yelmialmonte@gmail.com>
2020-10-03 13:25:23 -0400

    configure dotenv for environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 const cors = require("cors");
+
+dotenv.config();
 
 const app = express();
 app.use(cors())
@@ -17,4 +20,6 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`));
