2d3a2ec33467806c95cf2583e77f18cb8632761d
yimialmonte <yelmialmonte@gmail.com>
2020-10-03 15:28:22 -0400

    configure mongoose and connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import colors from 'colors'
 import  products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
+
 app.use(cors())
 app.get("/", (req, res) => {
   res.send("API send");
@@ -22,4 +26,4 @@ app.get("/api/products/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
