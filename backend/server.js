0a6711647fa3367962a274ced60c30ecf24235f9
yimialmonte <yelmialmonte@gmail.com>
2020-10-03 11:26:57 -0400

    get products from  backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const products = require("./data/products");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors())
 app.get("/", (req, res) => {
   res.send("API send");
 });
 
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.get("/products/:id", (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = products.find(p => p._id === req.params.id);
   res.json(product);
 });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('http://localhost:5000/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest products</h1>
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} key={product._id}>
-            <Product product={product}/>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
