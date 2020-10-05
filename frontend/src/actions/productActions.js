1faf1ea8563acca4de85de6d2365ec2a2f7aa2fc
yimialmonte <yelmialmonte@gmail.com>
2020-10-05 07:14:11 -0400

    create reducer action for products

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,22 @@
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from '../constants/productConstant';
+import axios from 'axios';
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get('http://localhost:5000/api/products');
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducer/productReducer';
 
-const reducer = combineReducers({})
-const initialState = {}
-const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const reducer = combineReducers({
+  productList: productListReducer
+});
+
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
