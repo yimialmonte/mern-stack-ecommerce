cb60a5c93ad3c4af4ea2fd9ceabfb7b88e6fce7b
yimialmonte <yelmialmonte@gmail.com>
2020-10-05 06:09:14 -0400

    create middleware for error handler

--- backend/routers/product.js
+++ backend/routers/product.js
@@ -1,28 +1,32 @@
-import express from 'express'
-import AsynHandler from 'express-async-handler'
-import Product from '../models/product.js'
+import express from 'express';
+import AsynHandler from 'express-async-handler';
+import Product from '../models/product.js';
 const router = express.Router();
-
 
 // @desc  Fetch all Product
 // @route GET /api/products
 // @access Public
-router.get("/", AsynHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}));
-
-
+router.get(
+  '/',
+  AsynHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc  Fetch single Product
 // @route GET /api/products/:id
 // @access Public
-router.get("/:id", AsynHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id);
+router.get(
+  '/:id',
+  AsynHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-  if(product) return res.json(product);
+    if (product) return res.json(product);
 
-  res.status(404).json({message: "Product not found"})
-}));
+    res.status(404);
+    throw new Error('Product not found');
+  })
+);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,17 @@ import cors from "cors";
 import colors from 'colors'
 import connectDB from "./config/db.js";
 import productRouter from './routers/product.js'
-
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors())
 app.use("/api/products", productRouter);
+
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
