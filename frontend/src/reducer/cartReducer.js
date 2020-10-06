c2445bdb1598cc06e1e5347f832be93bd5ed5cb4
yimialmonte <yelmialmonte@gmail.com>
2020-10-06 06:03:16 -0400

    added cart reducer and actions

---
+++ frontend/src/reducer/cartReducer.js
@@ -0,0 +1,29 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstant';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+      }
+      return {
+        ...state,
+        cartItems: [...state.cartItems, item],
+      };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(item => item !== action.payload.product)
+      }
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducer/productReducer';
+import {cartReducer} from './reducer/cartReducer'
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : []
+
+const initialState = {
+  cart: { cartImtes: cartItemsFromStorage }
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
