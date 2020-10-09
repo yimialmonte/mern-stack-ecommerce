e69031f6b649a2f1fec3470b34b22554a76c5ebb
yimialmonte <yelmialmonte@gmail.com>
2020-10-09 16:51:15 -0400

    create backend stuff for user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,4 +43,33 @@ const getUserProfile = AsynHandler(async (req, res) => {
   });
 });
 
-export { authUser, getUserProfile };
+// @desc  Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = AsynHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    res.status(201);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
