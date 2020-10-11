f1768f996da4ac733a44479c63af09e6633e2224
yimialmonte <yelmialmonte@gmail.com>
2020-10-11 16:15:44 -0400

    register user frontend functionallity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import ProductScreen from './screens/ProductScreen';
 import { Container } from 'react-bootstrap';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
 const App = () => {
   return (
     <Router>
@@ -14,6 +16,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/card/:id?" component={CartScreen} />
           <Route path="/" exact component={HomeScreen} />
