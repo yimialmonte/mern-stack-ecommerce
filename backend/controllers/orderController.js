ea1f8abcabbbc714117949d1789a4e7edea3f606
yimialmonte <yelmialmonte@gmail.com>
2020-10-17 06:35:20 -0400

    create controller for update order to paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,4 +52,29 @@ const getOrderById = AsynHandler(async (req, res) => {
 
 });
 
-export { addOrderItems, getOrderById };
+
+// @desc  Update order ro paid
+// @route PUT /api/orders/:id/pay
+// @access Private
+const updateOrderToPaid = AsynHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if(order) {
+   order.isPaid = true
+   order.paidAt = Date.now()
+   order.paymentResult = {
+     id: req.body.id,
+     status: req.body.status,
+     update_time: req.body.update_time,
+     email_address: req.body.player.email_address
+   }
+
+   const updateOrder = await order.save()
+   res.json(updateOrder)
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
