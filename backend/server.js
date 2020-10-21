bd0952170dd88bc95499c60dbce746c22740f7fe
yimialmonte <yelmialmonte@gmail.com>
2020-10-21 07:00:10 -0400

    create backend functionallity for uplaod image

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from 'path'
 import dotenv from "dotenv";
 import cors from "cors";
 import colors from 'colors'
@@ -6,6 +7,7 @@ import connectDB from "./config/db.js";
 import productRouter from './routers/product.js'
 import userRouter from './routers/userRoute.js'
 import orderRouter from './routers/orderRoute.js'
+import uploadRouter from './routers/uploadRouters.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config();
@@ -17,11 +19,14 @@ app.use(express.json());
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/upload", uploadRouter);
 app.use("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 });
 
+const __dirname = path.resolve()
 
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use(notFound)
 app.use(errorHandler)
 
